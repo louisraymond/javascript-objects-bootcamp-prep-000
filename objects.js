5dee07332ab20165ede4d5156caaf03bde59b1ca
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-05-20 03:43:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,5 @@
 var playlist={artistName:"song"};
 
-function updatePlaylist(playlist,artistName,songTitle) {
-  playlist.artistName= "songTitle"
-  return playlist
 }
 function updatePlaylist(playlist,artistName,songTitle){
 playlist[artistName]=songTitle
